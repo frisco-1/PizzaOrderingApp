4509734fc955f74f4789357c5e00960a824c8bb3
frisco-1 <paquito1214.2017@gmail.com>
2025-06-26 18:40:10 -0400

    Added Register page

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -4,9 +4,25 @@ export default function RegisterPage() {
   return (
    <>
     <section className='mt-8'>
-      <h1 className='text-center text-red-500 text-4xl'>
+      <h1 className='text-center text-red-500 text-4xl mb-4'>
         Register
       </h1>
+
+      <form className='block max-w-sm mx-auto'>
+        <input type="email" placeholder='email'/>
+        <input type="password" placeholder='password'/>
+        <button type="submit">Register</button>
+        <div className='my-4 text-center text-gray-500'>
+          or login with provider
+        </div>
+        <button className='flex gap-4 justify-center'>
+          {/* Place a Google icon with image */}
+          Login with Google
+        </button>
+      </form>
+
+     
+
     </section>
    </>
   )
